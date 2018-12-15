ab019eae9b8719449fd3c72b14fbe2a2a41f6c56
Ali Briceno <alienriquebm@gmail.com>
2018-12-15 14:10:20 -0400

    added express

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules": {
+        "linebreak-style": 0,
+        "no-use-before-define": ["error", { "functions": false, "classes": true }],
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+        "prefer-destructuring": ["error", { "object": false, "array": false }],
+        "linebreak-style": 0
+    }
+
 };
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const http = require('http');
+const colors = require('colors');
 
 http.createServer((req, res) => {
-  res.write('Hola mundo')
-})
+  res.write('Hola mundo');
+  res.end();
+}).listen(80);
+
+console.log(colors.rainbow('Listen port 80'));
