11132ab87b1eefd948eb78a7f62756ec8361bad8
Ali Briceno <alienriquebm@gmail.com>
2018-12-16 23:58:09 -0400

    added body parser and cors config

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const colors = require('colors');
 const express = require('express');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  next();
+});
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.json({
+    hello: 'world',
+  });
 });
 
 app.listen(process.env.SRV_PORT);
-console.log(colors.green(`Listen port ${process.env.SRV_PORT}`)); // eslint-disable-line
+console.log(colors.underline(`Listen port ${process.env.SRV_PORT}`)); // eslint-disable-line
