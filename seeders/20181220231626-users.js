e9c7b8f2c660db776673e0a766295731f0c20339
Ali Briceño <alienriquebm@gmail.com>
2018-12-20 23:31:10 -0400

    added bcrypt and seeder of user

---
+++ seeders/20181220231626-users.js
@@ -0,0 +1,21 @@
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: (queryInterface) => {
+    const plainPassword = 'admin';
+    const hashedPassword = bcrypt.hashSync(plainPassword, 10);
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@admin.com',
+      name: 'SPM',
+      lastname: 'Admin',
+      password: hashedPassword,
+      phone: null,
+      address: null,
+      isEnabled: 1,
+      createdAt: '2018-12-20 23:29:14',
+      updatedAt: '2018-12-20 23:29:14',
+    }], {});
+  },
+
+  down: queryInterface => queryInterface.bulkDelete('Users', null, {}),
+};
