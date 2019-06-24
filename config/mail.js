99ac564d9a5d108b6f8f4e32dde4526cc8da957a
Ali Briceño <alienriquebm@gmail.com>
2019-06-24 17:49:32 -0400

    end of mail implementation

---
+++ config/mail.js
@@ -0,0 +1,14 @@
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+const mail = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: process.env.MAIL_SECURE, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAIL_USER, // generated ethereal user
+    pass: process.env.MAIL_PASS, // generated ethereal password
+  },
+});
+
+module.exports = mail;
--- controllers/ContactUsController.js
+++ controllers/ContactUsController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const Validator = require('../common/Validator');
+const mail = require('../config/mail');
 
 const router = express.Router();
 
@@ -28,14 +29,31 @@ const processContact = async (req, res) => {
     data: `secret=${process.env.RECAPTCHA_SECRET}&response=${req.body.recaptcha}`,
     url: process.env.RECAPTCHA_URL_VERIFY,
   };
-  axios(options)
-    .then((response) => {
-      if (!response.data.success || response.data.score <= 0.5) {
-        return res.status(403).send('Sorry, only humans is allowed to send us a message.');
-      }
+
+  try {
+    const recaptchaVerify = await axios(options);
+    if (!recaptchaVerify.data.success || recaptchaVerify.data.score <= 0.5) {
+      return res.status(403).send('Sorry, only humans is allowed to send us a message.');
+    }
+    try {
+      const info = await mail.sendMail({
+        from: `"${body.data.name} <${body.data.email}>"`, // sender address
+        to: 'info@sanbricdesigns.com.ve', // list of receivers
+        subject: 'New contact from sanbricdesigns.com', // Subject line
+        text: body.data.message, // plain text body
+      });
+      // eslint-disable-next-line no-console
+      console.log('Message sent: %s', info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
       return res.status(200).send('Thank You! Your message has been sent.');
-    })
-    .catch(() => res.status(403).send('There was a problem with your submission, please try again.'));
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return res.status(403).send('There was a problem with your submission, please try again.');
+    }
+  } catch (error) {
+    return res.status(403).send('There was a problem with your submission, please try again.');
+  }
 };
 
 
